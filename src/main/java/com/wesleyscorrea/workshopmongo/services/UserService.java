6cd1731dde01ac843d423979b9b329bd4fd70232
unknown <Wesleyscorrea@hotmail.com>
2022-09-06 00:10:48 -0300

    Inserção de usuário com POST

--- src/main/java/com/wesleyscorrea/workshopmongo/services/UserService.java
+++ src/main/java/com/wesleyscorrea/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.wesleyscorrea.workshopmongo.services;
 
 import com.wesleyscorrea.workshopmongo.domain.User;
+import com.wesleyscorrea.workshopmongo.dto.UserDTO;
 import com.wesleyscorrea.workshopmongo.repository.UserRepository;
 import com.wesleyscorrea.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
