ebdc774301b21e8c891d52831df7807ae09abd8d
unknown <Wesleyscorrea@hotmail.com>
2022-09-07 14:05:56 -0300

    Consulta simples com query methods

--- src/main/java/com/wesleyscorrea/workshopmongo/resources/PostResource.java
+++ src/main/java/com/wesleyscorrea/workshopmongo/resources/PostResource.java
@@ -1,18 +1,13 @@
 package com.wesleyscorrea.workshopmongo.resources;
 
 import com.wesleyscorrea.workshopmongo.domain.Post;
-import com.wesleyscorrea.workshopmongo.domain.User;
-import com.wesleyscorrea.workshopmongo.dto.UserDTO;
+import com.wesleyscorrea.workshopmongo.resources.util.URL;
 import com.wesleyscorrea.workshopmongo.services.PostService;
-import com.wesleyscorrea.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -24,8 +19,15 @@ public class PostResource {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
-
         return ResponseEntity.ok().body(obj);
     }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
 }
 
---
+++ src/main/java/com/wesleyscorrea/workshopmongo/resources/util/URL.java
@@ -0,0 +1,8 @@
+package com.wesleyscorrea.workshopmongo.resources.util;
+
+public class URL {
+
+    public static String decodeParam(String text) {
+        return URL.decodeParam(text);
+    }
+}
