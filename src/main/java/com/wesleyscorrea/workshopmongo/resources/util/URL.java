85cb2c69d14083cf35d8f26dc2814afcd26cd117
unknown <Wesleyscorrea@hotmail.com>
2022-09-07 17:09:07 -0300

    Consulta simples com @Query

--- src/main/java/com/wesleyscorrea/workshopmongo/resources/util/URL.java
+++ src/main/java/com/wesleyscorrea/workshopmongo/resources/util/URL.java
@@ -1,8 +1,15 @@
 package com.wesleyscorrea.workshopmongo.resources.util;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 public class URL {
 
     public static String decodeParam(String text) {
-        return URL.decodeParam(text);
+        try {
+            return URLDecoder.decode(text, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return "";
+        }
     }
 }
